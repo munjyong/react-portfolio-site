32a4a5c0e50ed23ea609c155de4a71588736d774
Edmund <munjyong@gmail.com>
2019-11-18 14:01:51 +0000

    Change Favicon

--- src/data.js
+++ src/data.js
@@ -12,7 +12,7 @@ export default
                         'from England 🇬🇧'
         ],
         //Contact Email
-        contactEmail:'munjyong@gmail.com',
+        contactEmail:'',
         // Add Your About Text Here
         abouttext: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
         aboutImage:'',
@@ -20,14 +20,14 @@ export default
        ShowAboutImage:true,//true or false (Change Here)
        // Change Projects Here 
        projects:[
-           {
-            id: 1,//DO NOT CHANGE THIS 
-            title:'Project One', //Project Title - Add Your Project Title Here
-             service:'UI/UX Design', // Add Your Service Type Here
-            //Project Image - Add Your Project Image Here
-             imageSrc:"https://images.unsplash.com/photo-1558104631-0fa41a8f6c20?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
-             //Project URL - Add Your Project Url Here
-             url:'http://chetanverma.com/'
+            {
+                id: 1,//DO NOT CHANGE THIS 
+                title:'Project One', //Project Title - Add Your Project Title Here
+                service:'UI/UX Design', // Add Your Service Type Here
+                //Project Image - Add Your Project Image Here
+                imageSrc:"https://images.unsplash.com/photo-1558104631-0fa41a8f6c20?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+                //Project URL - Add Your Project Url Here
+                url:'http://chetanverma.com/'
             },
             {
                 id: 2,//DO NOT CHANGE THIS 
