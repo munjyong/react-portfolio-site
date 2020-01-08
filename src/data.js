325bde0550ef35067b168095492c8fee8ea3a4ac
Edmund <munjyong@gmail.com>
2020-01-08 21:07:27 +0000

    Add chatroom project

--- src/data.js
+++ src/data.js
@@ -31,25 +31,11 @@ export default
             },
             {
                 id: 2,//DO NOT CHANGE THIS 
-                title: 'Persistent Todo List',
-                service: 'Created with React & localStorage store local data',
-                imageSrc: "https://images.unsplash.com/photo-1554866585-cd94860890b7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
-                url: 'http://chetanverma.com/'
-            },
-            { 
-                id: 3,//DO NOT CHANGE THIS 
-                title: 'Project Three',
-                service: 'Web App',
-                imageSrc: "https://images.unsplash.com/photo-1511500118080-275313ec90a1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
-                url: 'http://chetanverma.com/'
-            },
-            {
-                id: 4,//DO NOT CHANGE THIS
-                title: 'Project Four',
-                service: 'Branding',
-                imageSrc: "https://images.unsplash.com/photo-1558452919-08ae4aea8e29?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
-                url: 'http://chetanverma.com/'
-           }
+                title: 'Real-Time Chatroom',
+                service: 'Created with React & ChatKit API from Pusher',
+                imageSrc: "./chatroom.gif",
+                url: 'https://github.com/munjyong/chatroom-app'
+            }
         ],
         social: [
             // Add Or Remove The Link Accordingly
