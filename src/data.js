47e5677cf051a293a61f593171479d18e550c27c
Edmund <munjyong@gmail.com>
2020-01-21 20:56:35 +0000

    Edit bio

--- src/data.js
+++ src/data.js
@@ -14,7 +14,7 @@ export default
         //Contact Email
         contactEmail:'munjyong@gmail.com',
         // Add Your About Text Here
-        abouttext: "I am a recent Computer Science graduate seeking to kickstart my web development career. My passion for learning drives me to challenge myself, experiment with new tools and most important... to create. The projects on this site demonstrate skills I have utilized through self-study and online courses, with each project presenting unique challenges I hope keep on diversifying this portfolio in the near future.",
+        abouttext: "I am a recent Computer Science graduate seeking to kickstart my web development career. My passion for learning drives me to challenge myself, experiment with new tools and most importantly... create. The projects on this site demonstrate skills I have utilized through self-study and online courses, with each project presenting unique challenges I hope to keep on diversifying this portfolio soon.",
         aboutImage:'',
        //Change This To Hide The Image of About Section (True Or False)
         ShowAboutImage:true,//true or false (Change Here)
